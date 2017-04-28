842cb3d1825a122b409cbaab275a2b26d0b63317
Gabriel Mangiurea <mangiurea.gabriel@gmail.com>
2017-04-28 14:05:20 +0300

    Fix: bad command in execa (test.js)

--- test.js
+++ test.js
@@ -4,6 +4,6 @@ const execa = require('execa');
 const test = require('ava');
 
 test('failure: not specifying the name of the library', async t => {
-	await execa.stdout('latest-lib', [''])
+	await execa.stdout('node', ['./cli.js', ''])
 	.then(response => t.is(/Please specify the name of the library/.test(response), true));
 });
